f7c715b31cf49ceb03c8a124eb1655470dc882cf
dyh <805475699@qq.com>
2022-02-18 20:14:20 +0800

    测试分支使用

--- ruoyi-common/src/main/java/com/ruoyi/common/config/RuoYiConfig.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/config/RuoYiConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * 读取项目相关配置
+ * 读取项目相关配置 aaaaa
  * 
  * @author ruoyi
  */
